e2cde9b2e33e27c0af7c7df972a2a76c6cccf25a
Marcos Andrade <marcospandrade2@gmail.com>
2023-08-17 00:24:14 -0300

    feat: create auth guard to check protected routes and look for valid accessTokens

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Get, Request, HttpCode, HttpStatus } from '@nestjs/common';
 
 import { IAuth } from './dto/login.dto';
 import { AuthUseCase } from './use-cases/auth.use-cases';
+import { RequestUser } from 'src/shared/helpers/generic.helpers';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authUseCase: AuthUseCase) {}
 
-  @Post('/login')
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
   public login(@Body() body: IAuth) {
     return this.authUseCase.login(body);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req: RequestUser) {
+    return req.user;
   }
 }
