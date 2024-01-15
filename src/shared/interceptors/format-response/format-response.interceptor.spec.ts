3be36e9644751ad8db89a41f82517339fa58ba9b
Marcos Andrade <marcospandrade2@gmail.com>
2024-01-14 22:20:52 -0300

    refactor: cleaning server folder

--- src/shared/interceptors/format-response/format-response.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FormatResponseInterceptor } from './format-response.interceptor';
-
-describe('FormatResponseInterceptor', () => {
-    it('should be defined', () => {
-        expect(new FormatResponseInterceptor()).toBeDefined();
-    });
-});
